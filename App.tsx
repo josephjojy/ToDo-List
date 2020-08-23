54956b54c8a388dc93c932ceb9501faa8a889375
Joseph Jojy Chirayath <33942648+josephjojy@users.noreply.github.com>
2020-08-23 23:41:43 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import Todos from './Todos'
+import AddTodo from './AddTodo'
+
+class App extends Component{
+
+state={
+  i:1,
+  todos:[
+    
+  ]
+}
+checktodo =(id:number) =>{
+  const todos = this.state.todos.filter((todo:any) =>{
+    return todo.id!==id;
+  });
+  this.setState({
+    todos
+  })
+}
+
+addTodo = (todo:any) => {
+  todo.id=this.state.i;
+  let i = todo.id + 1;
+  let todos = [...this.state.todos,todo];
+  this.setState({
+    i,
+    todos
+  })
+
+}
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Todo List</h1>
+        <h3><Todos todos={this.state.todos} checktodo={this.checktodo}/></h3>
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+    );
+  }
+}
+export default App
